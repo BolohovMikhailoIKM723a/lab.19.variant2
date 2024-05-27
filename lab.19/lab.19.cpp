75f5964c823cb86dd20e25814f97dfb8bf35e685
Михаил <@DESKTOP-1RCLF6R>
2024-05-27 14:52:32 +0300

    Add project files.

---
+++ lab.19/lab.19.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+template <typename T>
+void calculate_powers(T value, T& square, T& cube, T& fourth_power) {
+    square = value * value;
+    cube = square * value;
+    fourth_power = cube * value;
+}
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int value = 3;
+    int square, cube, fourth_power;
+    calculate_powers(value, square, cube, fourth_power);
+    cout << "квадрат: " << square << endl;
+    cout << "куб: " << cube << endl;
+    cout << "четверта ступінь: " << fourth_power << endl;
+    return 0;
+}
